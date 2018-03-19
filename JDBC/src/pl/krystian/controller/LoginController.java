9541fc41ca97eaac119005db935067b5fa30126e
Krystian <klukasiak@sigma.ug.edu.pl>
2018-03-19 17:52:41 +0100

    JDBC login panel layout added

---
+++ JDBC/src/pl/krystian/controller/LoginController.java
@@ -0,0 +1,42 @@
+package pl.krystian.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private TextField usernameTextfield;
+
+    @FXML
+    private Label passwordLabel;
+
+    @FXML
+    private PasswordField passwordTextField;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private CheckBox rememberCheckbox;
+
+    @FXML
+    private Label rememberLabel;
+
+    @FXML
+    private Label registerLabel;
+
+    @FXML
+    private Button registerButton;
+
+}
