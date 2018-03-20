fdf3bdd1bbfc2bbb8ea50a365a39c18b6387e6e2
Krystian <klukasiak@sigma.ug.edu.pl>
2018-03-20 02:13:59 +0100

    Log in included

--- JDBC/src/pl/krystian/DBConnection.java
+++ JDBC/src/pl/krystian/DBConnection.java
@@ -2,21 +2,13 @@ package pl.krystian;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
-	public Connection connect() {
-		Connection c = null;
-		try {
-			Class.forName("org.postgresql.Driver");
-			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres",
-					"qwerty");
-
-		} catch (Exception e) {
-			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-		}
-		
-		System.out.println("Podlaczono do bazy :)");
-		return c;
-		
+	public Connection connect(String username, String password) throws ClassNotFoundException, SQLException {
+		Connection conn = null;
+		Class.forName("org.postgresql.Driver");
+		conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", username, password);
+		return conn;
 	}
 }
--- JDBC/src/pl/krystian/controller/LoginController.java
+++ JDBC/src/pl/krystian/controller/LoginController.java
@@ -1,80 +1,77 @@
 package pl.krystian.controller;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import pl.krystian.DBConnection;
 
-public class LoginController{
+public class LoginController {
 
-    @FXML
-    private Label titleLabel;
+	@FXML
+	private Label titleLabel;
 
-    @FXML
-    private Label usernameLabel;
+	@FXML
+	private Label usernameLabel;
 
-    @FXML
-    private TextField usernameTextfield;
+	@FXML
+	private TextField usernameTextField;
 
-    @FXML
-    private Label passwordLabel;
+	@FXML
+	private Label passwordLabel;
 
-    @FXML
-    private PasswordField passwordTextField;
+	@FXML
+	private PasswordField passwordTextField;
 
-    @FXML
-    private Button signInButton;
+	@FXML
+	private Button signInButton;
 
-    @FXML
-    private CheckBox rememberCheckbox;
+	@FXML
+	private CheckBox rememberCheckbox;
 
-    @FXML
-    private Label rememberLabel;
+	@FXML
+	private Label rememberLabel;
 
-    @FXML
-    private Label registerLabel;
+	@FXML
+	private Label registerLabel;
 
-    @FXML
-    private Button registerButton;
+	@FXML
+	private Button registerButton;
 
-    @FXML
-    private void test(ActionEvent event) {
-    	DBConnection conn = new DBConnection ();
-    	try {
-    		Statement stmt = conn.connect().createStatement();
-    	    String sql;
-    	    sql = "SELECT * from users";
-    	    ResultSet rs = stmt.executeQuery(sql);
-    	    while(rs.next()){
-    	    	int id  = rs.getInt("id_user");
-    	        int age = rs.getInt("age");
-    	        String first = rs.getString("firstname");
-    	        String last = rs.getString("lastname");
-    	        String nick = rs.getString("nickname");
-    	        String city = rs.getString("city");
+	@FXML
+	private Label connectionLabel;
 
-    	        System.out.print("User ID: " + id);
-    	        System.out.print(", Age: " + age);
-    	        System.out.print(", First Name: " + first);
-    	        System.out.print(", Last Name: " + last);
-    	        System.out.print(", Nickname: " + nick);
-    	        System.out.print(", City: " + city);
-    	      }
-    	      //STEP 6: Clean-up environment
-    	      rs.close();
-    	      stmt.close();
-    	} catch (Exception e) {
+	@FXML
+	private void test(ActionEvent event) {
+		DBConnection conn = new DBConnection();
+		String username = String.valueOf(usernameTextField.getText());
+		String password = String.valueOf(passwordTextField.getText());
+
+		try {
+			conn.connect(username, password);
+
+			Parent root;
+
+			root = FXMLLoader.load(getClass().getClassLoader().getResource("pl/krystian/view/MainPane.fxml"));
+			Stage stage = new Stage();
+			stage.setTitle("SQL CRUD Panel - LOGGED IN");
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+			((Node) (event.getSource())).getScene().getWindow().hide();
+		} catch (Exception e) {
+			connectionLabel.setText("B³edne dane :(");
+			connectionLabel.setTextFill(Color.RED);
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(0);
 		}
-    }
+	}
 }
-
